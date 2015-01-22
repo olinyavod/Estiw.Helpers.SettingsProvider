9a0b47c20ade105175c4c6ea84abd15353b94c4d
olinyavod <zhuravlev@estiw.ru>
2015-01-22 16:00:07 +1000

    Init solution

---
+++ src/Estiw.Helpers.SettingsProvider/Estiw.Helpers.SettingsProvider/ISettingsProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estiw.Helpers.SettingsProvider
+{
+    public interface ISettingsProvider
+    {
+    }
+}
